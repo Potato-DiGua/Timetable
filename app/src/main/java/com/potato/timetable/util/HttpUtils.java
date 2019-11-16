9128e7100e15f6949c82f420804c41b8fe8b1a79
zzl <86543402@qq.com>
2019-11-16 19:18:37 +0800

    update

--- app/src/main/java/com/potato/timetable/util/HttpUtils.java
+++ app/src/main/java/com/potato/timetable/util/HttpUtils.java
@@ -19,24 +19,25 @@ import java.util.List;
 
 public class HttpUtils {
 
+    /**
+     * ping 判断网络连通
+     * @return
+     */
     public static boolean isNetworkConnected() {
         try {
             //代表ping 3 次 超时时间为10秒
             Process p = Runtime.getRuntime().exec("ping -c 3 -w 10 www.baidu.com");//ping3次
 
             int status = p.waitFor();
-
             if (status == 0) {
                 //代表成功
                 return true;
-            } else {
-                //代表失败
-                return false;
             }
         } catch (Exception e) {
            e.printStackTrace();
-           return false;
         }
+
+        return false;
     }
 
     public static String sendGet(String strUrl) {
