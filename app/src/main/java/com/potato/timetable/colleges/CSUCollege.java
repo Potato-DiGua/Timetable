d1d63d07bbfc09f1071e823c20dcaa50e2fbd5ff
zzl <86543402@qq.com>
2021-03-16 21:35:36 +0800

    BugFix:修复中南大学接口

--- app/src/main/java/com/potato/timetable/colleges/CSUCollege.java
+++ app/src/main/java/com/potato/timetable/colleges/CSUCollege.java
@@ -4,10 +4,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.text.TextUtils;
 
-import com.potato.timetable.MyApplication;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
 import com.potato.timetable.bean.Course;
 import com.potato.timetable.colleges.base.College;
-import com.potato.timetable.util.ExcelUtils;
 import com.potato.timetable.util.OkHttpUtils;
 
 import org.jsoup.Jsoup;
@@ -16,6 +17,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import okhttp3.FormBody;
@@ -30,8 +33,10 @@ public class CSUCollege implements College {
     private static final String LOGIN_URL = BASE_URL + "/Logon.do?method=logon";
     private static final String RANDOM_CODE_URL = BASE_URL + "/verifycode.servlet";
     private static final String INDEX_URL = BASE_URL + "/jsxsd/framework/xsMain.jsp";
-    private static final String TIMETABLE_EXCEL_URL = BASE_URL + "/jsxsd/xskb/xskb_print.do?xnxq01id=%s&zc=";
-    private String[] termOptions;
+    // 课程表json
+    private static final String TIMETABLE_JSON_URL = BASE_URL + "/jsxsd/kbxx/getKbxx.do";
+    // 我的课表
+    private static final String TERMS_URL = BASE_URL + "/jsxsd/xskb/xskb_list.do?Ves632DSdyV=NEW_XSD_WDKB";
 
 
     @Override
@@ -58,16 +63,7 @@ public class CSUCollege implements College {
             String result = OkHttpUtils.downloadText(request);
             if (!TextUtils.isEmpty(result)) {
                 Document doc = Jsoup.parse(result);
-                if (doc.title().equals("学生个人中心")) {
-                    Element e = doc.select("select[id=xnxq01id]").first();
-                    Elements es = e.children();
-                    termOptions = new String[es.size()];
-                    int i = 0;
-                    for (Element element : es) {
-                        termOptions[i++] = element.text();
-                    }
-                    return true;
-                }
+                return doc.title().equals("学生个人中心");
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -81,16 +77,7 @@ public class CSUCollege implements College {
         String result = OkHttpUtils.downloadText(INDEX_URL);
         if (!TextUtils.isEmpty(result)) {
             Document doc = Jsoup.parse(result);
-            if (doc.title().equals("学生个人中心")) {
-                Element e = doc.select("select[id=xnxq01id]").first();
-                Elements es = e.children();
-                termOptions = new String[es.size()];
-                int i = 0;
-                for (Element element : es) {
-                    termOptions[i++] = element.text();
-                }
-                return true;
-            }
+            return doc.title().equals("学生个人中心");
         }
         return false;
     }
@@ -133,45 +120,178 @@ public class CSUCollege implements College {
 
     @Override
     public List<Course> getCourses(String term) {
-        String strUrl = String.format(TIMETABLE_EXCEL_URL, term);
         FormBody form = new FormBody.Builder()
                 .add("xnxq01id", term)
                 .add("zc", "")
                 .build();
         Request request = new Request.Builder()
-                .url(strUrl)
+                .url(TIMETABLE_JSON_URL)
                 .post(form)
                 .build();
+        String json = OkHttpUtils.downloadText(request);
+        List<TimetableItem> list = new Gson().fromJson(json, new TypeToken<List<TimetableItem>>() {
+        }.getType());
+        List<Course> courseList = new ArrayList<>(list.size());
+        for (TimetableItem item : list) {
+            if (item.lesson > 6) {
+                continue;
+            }
+            for (String content : item.title.split("\n\n")) {
+                //课程名称：大学英语（一）\n上课教师：黄莹讲师（高校）\n周次：5-19(周)\n星期：星期一\n节次：0102节\n上课地点：外语网络楼449\n
+                String[] lines = content.split("\n");
+                if (lines.length != 6) {
+                    continue;
+                }
+                Course course = new Course();
+                course.setName(getValue(lines[0]));
+                course.setTeacher(getValue(lines[1]));
+                course.setClassRoom(getValue(lines[5]));
+                course.setClassStart((item.lesson - 1) * 2 + 1);
+                String str = getValue(lines[4]);// 0102节
+                str = str.substring(0, str.length() - 1);//0102
+                try {
+                    int min = Integer.parseInt(str.substring(0, 2));
+                    int max = Integer.parseInt(str.substring(str.length() - 2));
+                    course.setClassLength(max - min + 1);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                    course.setClassLength(2);
+                }
 
-        String path = MyApplication.getApplication().getFilesDir().getAbsolutePath();
-        String name = "timetable.xls";
-//        Log.d("excel", path + "/" + name);
+                int week = item.DayOfWeek - 1;
+                if (week == 0) {
+                    week = 7;
+                }
+                course.setDayOfWeek(week);
 
-        if (OkHttpUtils.downloadToLocal(request, path, name)) {
-            File file = new File(path, name);
-            if (file.exists()) {
-                List<Course> list = ExcelUtils.handleExcel(file.getAbsolutePath(), 4, 2, new ExcelUtils.HandleResult() {
-                    @Override
-                    public Course handle(String courseStr, int row, int col) {
-                        Course course = ExcelUtils.getCourseFromString(courseStr);
-                        if (course == null)
-                            return null;
-                        course.setDayOfWeek(col == 1 ? 7 : col - 1);
-                        course.setClassStart(row * 2 - 1);
-                        return course;
-                    }
-                });
-                file.delete();
-                return list;
+                course.setWeekOfTerm(getWeekOfTermFromString(getValue(lines[2])));
+                courseList.add(course);
             }
         }
-        return null;
+        return courseList;
 
+    }
+
+    private int getWeekOfTermFromString(String text) {
+        //Log.d("excel",text);
+        String[] s1 = text.trim().split("\\(");
+        String[] s11 = s1[0].split(",");
+
+        int weekOfTerm = 0;
+        for (String s : s11) {
+            if (s == null || s.isEmpty())
+                continue;
+            if (s.contains("-")) {
+                int space = 2;
+                if (text.contains("(周)")) {
+                    space = 1;
+                }
+                String[] s2 = s.split("-");
+                if (s2.length != 2) {
+                    return 0;
+                }
+                int min = Integer.parseInt(s2[0]);
+                int max = Integer.parseInt(s2[1]);
+                if (text.contains("单") && min % 2 == 0) {
+                    min++;
+                } else if (text.contains("双") && min % 2 == 1) {
+                    min++;
+                }
+
+                for (int n = min; n <= max; n += space) {
+                    weekOfTerm += 1 << (25 - n);
+                }
+            } else {
+                weekOfTerm += 1 << (25 - Integer.parseInt(s));
+            }
+        }
+        return weekOfTerm;
+    }
+
+    private String getValue(String line) {
+        return line.substring(line.indexOf('：') + 1).trim();
+    }
+
+    /*        "jc": 1,
+     "title": "课程名称：大学英语（一）\n上课教师：黄莹讲师（高校）\n周次：5-19(周)\n星期：星期一\n节次：0102节\n上课地点：外语网络楼449\n",
+     "xq": 2,
+     "kcmc": "大学英语（..."
+
+     */
+    private static class TimetableItem {
+        /**
+         * 第几节课
+         * 值[1,7]
+         * 7表示备注
+         */
+        @SerializedName("jc")
+        private int lesson;
+        /**
+         * 星期几
+         * 数值1-7
+         * 1表示周日，依次类推
+         */
+        @SerializedName("xq")
+        private int DayOfWeek;
+        private String title;
+        private String kcmc;
+
+        public int getLesson() {
+            return lesson;
+        }
+
+        public TimetableItem setLesson(int lesson) {
+            this.lesson = lesson;
+            return this;
+        }
+
+        public int getDayOfWeek() {
+            return DayOfWeek;
+        }
+
+        public TimetableItem setDayOfWeek(int dayOfWeek) {
+            DayOfWeek = dayOfWeek;
+            return this;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public TimetableItem setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public String getKcmc() {
+            return kcmc;
+        }
+
+        public TimetableItem setKcmc(String kcmc) {
+            this.kcmc = kcmc;
+            return this;
+        }
     }
 
     @Override
     public String[] getTermOptions() {
-        return termOptions;
+        List<String> termOptions = new LinkedList<>();
+        try {
+            String result = OkHttpUtils.downloadText(TERMS_URL);
+            if (!TextUtils.isEmpty(result)) {
+                Document doc = Jsoup.parse(result);
+                Element e = doc.select("#xnxq01id").first();
+                Elements es = e.children();
+                int i = 0;
+                for (Element element : es) {
+                    termOptions.add(element.text().trim());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return termOptions.toArray(new String[0]);
     }
 
     @Override
